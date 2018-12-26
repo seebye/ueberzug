a49507cf6bfce2748d6ee0ab7f49c67248b1cc53
seebye <seebye@users.noreply.github.com>
2018-12-26 21:20:18 +0100

    force to include shell scripts in packages

--- setup.py
+++ setup.py
@@ -27,6 +27,9 @@ setup(
     license='GPLv3',
 
     include_package_data=True,
+    package_data={
+        '': ['*.sh'],
+    },
     entry_points={
         'console_scripts': [
             'ueberzug=ueberzug.ueberzug:main'
@@ -39,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='13',  # Required
+    version='14',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
