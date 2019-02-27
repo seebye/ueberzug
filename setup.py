b8517801666964d22b58fdae4390864c31e9c07d
seebye <seebye@users.noreply.github.com>
2019-02-27 21:03:46 +0100

    use Semantic Versioning, new release
    See: https://semver.org/

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='18',  # Required
+    version='18.1.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
