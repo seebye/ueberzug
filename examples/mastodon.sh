fb9e145ee3b6afc8e4d5134fd48551137967d815
seebye <seebye@users.noreply.github.com>
2019-07-06 15:15:55 +0200

    mention the license in the example scripts
    Well, it's already stated that every file in this project is licensed
    with the GPLv3,
    but I will make it a little more obvious that the examples are also
    gplv3 licensed.

--- examples/mastodon.sh
+++ examples/mastodon.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+# This is just an example how ueberzug can be used.
+# Copyright (C) 2019  Nico Baeurer
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 source "`ueberzug library`"
 
 readonly USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
