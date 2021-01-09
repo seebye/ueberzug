08fb2454671917cf82dbc4d13a458c6226fc1657
seebye <seebye@users.noreply.github.com>
2021-01-09 12:39:14 +0100

    release new version

--- ueberzug/__init__.py
+++ ueberzug/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '18.1.8'
+__version__ = '18.1.9'
 __license__ = 'GPLv3'
 __description__ ='ueberzug is a command line util which allows to display images in combination with X11'
 __url_repository__ = 'https://github.com/seebye/ueberzug'
